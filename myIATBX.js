e415dc54285b5ed5ef66156192d3efdcf9eeb6ef
francksa <fsarrazit@gmail.com>
2020-09-09 14:21:50 -0400

    Update myIATBX.js

--- myIATBX.js
+++ myIATBX.js
@@ -3,37 +3,37 @@ define(['pipAPI', 'https://francksa.github.io/testing/quiat8.js'], function(APIC
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'Coke', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Coke'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {image : 'coke1.jpg'}, 
+    			{image : 'coke2.jpg'}, 
+    			{image : 'coke3.jpg'}, 
+    			{image : 'coke4.jpg'}, 
+    			{image : 'coke5.jpg'}, 
+    			{image : 'coke6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'Pepsi', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : 'Pepsi'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		    {image : 'pepsi1.jpg'}, 
+    			{image : 'pepsi4.jpg'}, 
+    			{image : 'pepsi5.jpg'}, 
+    			{image : 'pepsi2.jpg'}, 
+    			{image : 'pepsi3.jpg'}, 
+    			{image : 'pepsi6.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
